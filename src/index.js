90c4bcbc45b96472a2410e3799f58c66dbd3a531
Şeref Kaynak <serefkynk@gmail.com>
2023-10-01 13:47:38 +0300

    Mouseover added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import './less/index.less'
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
+document.querySelector("h1").addEventListener("click",function(e){
+alert("Bana tıkladın!")
+});
+
+// document.getElementsByClassName("nav-link").addEventListener("mouseover",mouseOver);
+// document.getElementsByClassName("nav-link").addEventListener("mouseout",mouseOut);
+// function mouseOver() {
+//     document.getElementsByClassName("nav-link").style.color = "red";
+//   }
+//   function mouseOut() {
+//     document.getElementsByClassName("nav-link").style.color = "black";
+//   }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let navLinks = document.getElementsByClassName("nav-link");
+
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener("mouseover", mouseOver);
+        navLinks[i].addEventListener("mouseout", mouseOut);
+    }
+    function mouseOver() {this.style.color = "red";}
+    function mouseOut() {this.style.color = "black";}
+});
 
 
-// Kodlar buraya gelecek!
